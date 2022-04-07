ca0fda709dec5dae8f13ca87e3df270a86f36eab
Nate M <jeditendencies35@gmail.com>
2022-04-07 16:47:59 -0400

    added reducers and moved and modified fetchUsers function

--- src/components/layout/Spinner.jsx
+++ src/components/layout/Spinner.jsx
@@ -4,7 +4,7 @@ function Spinner() {
   return (
     <div className="w-100 mt-20">
       <img
-        width={300}
+        width={450}
         className="text-center mx-auto"
         src={spinner}
         alt="Loading..."
